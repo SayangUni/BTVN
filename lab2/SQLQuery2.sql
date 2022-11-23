f460568766321b047c90f49ead11db95387b92cd
pham tuan <89393220+P-TUAN@users.noreply.github.com>
2022-11-23 15:38:31 +0700

    Add files via upload

---
+++ lab2/SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿use QLDA1
+SELECT (HONV+ ' ' +TENLOT+ ' ' +TENNV) AS 'Họ Tên Nhân Viên Có Lương Trên Mức Lương Trung Bình Của Phòng "Nghiên cứu"'
+FROM NHANVIEN
+WHERE LUONG > (SELECT AVG(LUONG)
+FROM NHANVIEN, PHONGBAN
+WHERE PHG = MAPHG AND TENPHG = N'Nghiên cứu')
---
+++ lab2/Tínhcv,dt.sql
@@ -0,0 +1,7 @@
+declare @d float, @r float, @cv float, @dt float;
+set @d=2
+set @r=3
+set @cv=(@d+@r)*2;
+set @dt=@d*@r;
+select 'Chu vi=',@cv;
+select 'Dien tich=',@dt;
